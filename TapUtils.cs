08b959ec4a3c908f14078cd5b248b6c1f475488a
Timur Galiullin <androidsmallmeat@gmail.com>
2020-11-13 15:05:10 +0500

    Init repo

---
+++ TapUtils.cs
@@ -0,0 +1,20 @@
+﻿//  USAGE: https://github.com/PhatIdler/SAB-Games-Utils
+
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace SabGames.Utils
+{
+    public class TapUtils
+    {
+        public static bool IsPointerOverUIObject()
+        {
+            PointerEventData eventDataCurrentPosition = new PointerEventData(EventSystem.current);
+            eventDataCurrentPosition.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            List<RaycastResult> results = new List<RaycastResult>();
+            EventSystem.current.RaycastAll(eventDataCurrentPosition, results);
+            return results.Count > 0;
+        }
+    }
+}
